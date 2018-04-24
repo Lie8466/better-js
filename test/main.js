628ff28bc3ac3a7b95e3078ecfc2425adcb32172
Yawei Li <liyawei04@meituan.com>
2018-04-24 13:24:41 +0800

    test文件，错误日志log

---
+++ test/main.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var watch = require('node-watch');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method=="OPTIONS") res.sendStatus(200);/*让options请求快速返回*/
+    else  next();
+});
+app.post('/sendError', function (req, res) {
+    var body = req.body;
+    try {
+        var fd = fs.openSync('log.txt', 'a+');
+        fs.appendFileSync(fd, JSON.stringify(body) + '\r\n=======\r\n');
+        res.send({message: '成功'});
+      } catch (err) {
+        /* Handle the error */
+        res.send({message: '失败', error: err});
+      }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+watch(__dirname + '/', { recursive: true }, function(evt, name) {
+    console.log('%s changed.', name);
+  });
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+  
+    console.log('Example app listening at http://%s:%s', host, port);
+});
